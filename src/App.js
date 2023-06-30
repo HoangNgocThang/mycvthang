2e3bcb596b04ef6943293829b6cc7fdca2d623e3
HoangNgocThang <thanghai.tb123@gmail.com>
2023-06-30 15:37:02 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React By THANG-HOANG-NGOC
         </a>
       </header>
     </div>
